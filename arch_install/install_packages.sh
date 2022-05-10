6d6068e92046e807262f4b60746b83dadc00d5ce
Fernando Okuma <feokuma@gmail.com>
2022-05-10 19:19:58 -0300

    :bug: Fix yay install script

--- arch_install/install_packages.sh
+++ arch_install/install_packages.sh
@@ -2,7 +2,14 @@
 
 sudo pacman-mirrors
 sudo pacman -Syyu --no-confirm
-sudo pacman -S yay --no-confirm
+
+cd /opt
+sudo git clone https://aur.archlinux.org/yay-git.git
+sudo chown -R $USER:$USER ./yay-git
+cd yay-git
+makepkg -si
+cd $HOME
+
 yay -S base-devel 
 yay -S $(cat install_packages.txt | xargs)
 npm i -g $(cat npm_packages.txt | xargs)
