44af2808ccc96a3acdc98a6d7e0d3ef4f72a2826
Fernando Okuma <feokuma@gmail.com>
2024-11-22 20:35:58 -0300

    🔧 Adiciona DAP e configurações para dotnet

--- nvim/.config/nvim/lua/plugins/debugging.lua
+++ nvim/.config/nvim/lua/plugins/debugging.lua
@@ -2,8 +2,44 @@
 return {
   {
     "mfussenegger/nvim-dap",
+
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+    },
+
     config = function()
       local dap = require("dap")
+
+      dap.adapters.coreclr = {
+        type = "executable",
+        command = "/home/feokuma/.local/share/nvim/mason/packages/netcoredbg/netcoredbg",
+        args = { "--interpreter=vscode" },
+      }
+
+      dap.configurations.cs = {
+        {
+          type = "coreclr",
+          request = "launch",
+          name = "Launch .NET Core",
+          program = function()
+            local cwd = vim.fn.getcwd()
+            local project_files = vim.fn.glob(cwd .. "/*.csproj", false, true)
+            if #project_files == 0 then
+              error("No .csproj file found in the workspace.")
+            end
+            local project_name = vim.fn.fnamemodify(project_files[1], ":t:r")
+
+            local dll_path = vim.fn.glob(cwd .. "/bin/Debug/**/" .. project_name .. ".dll")
+            if dll_path == "" then
+              return vim.fn.input("Path to DLL > ", cwd .. "/", "file")
+            end
+
+            return dll_path
+          end,
+          cwd = "${workspaceFolder}",
+          stopAtEntry = false,
+        },
+      }
 
       dap.adapters.node2 = {
         type = "executable",
