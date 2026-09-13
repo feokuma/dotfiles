310002890c2f18a24dcf2aa263a07bff6ba7fc18
feokuma <feokuma@gmail.com>
2026-09-13 00:24:56 +0000

    🔧 hypr: add modular Lua configuration

---
+++ hypr/.config/hypr/input.lua
@@ -0,0 +1,29 @@
+hl.config({
+    input = {
+        kb_layout  = "us",
+        kb_variant = "intl",
+        kb_model   = "",
+        kb_options = "",
+        kb_rules   = "",
+
+        follow_mouse = 1,
+
+        sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
+
+        touchpad = {
+            natural_scroll = true,
+	    tap_to_click=false,
+        },
+    },
+})
+
+hl.gesture({
+    fingers = 3,
+    direction = "horizontal",
+    action = "workspace",
+})
+
+hl.device({
+    name        = "epic-mouse-v1",
+    sensitivity = -0.5,
+})
---
+++ hypr/.config/hypr/programs.lua
@@ -0,0 +1,11 @@
+local terminal    = "ghostty"
+local fileManager = "dolphin"
+local menu        = "hyprlauncher"
+local browser     = "firefox"
+
+return {
+    terminal    = terminal,
+    fileManager = fileManager,
+    menu        = menu,
+    browser     = browser
+}
