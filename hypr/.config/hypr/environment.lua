e7e4382da8f7b32c533c05c55d539b3db0ec4591
feokuma <feokuma@gmail.com>
2026-09-14 18:42:57 -0300

    🔧 hypr: force GTK dark theme via session environment

--- hypr/.config/hypr/environment.lua
+++ hypr/.config/hypr/environment.lua
@@ -1,3 +1,5 @@
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("GTK_IM_MODULE", "simple")
+-- Force GTK apps (incl. portal file chooser used by Chrome) to follow dark theme
+hl.env("GTK_THEME", "Adwaita-dark")
