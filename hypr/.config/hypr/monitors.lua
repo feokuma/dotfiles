286f25f447fd60669ee90f0df11c1ecb1df6e40b
feokuma <feokuma@gmail.com>
2026-09-16 21:27:15 -0300

    🐛 hypr: fix fractional scale on eDP-1

--- hypr/.config/hypr/monitors.lua
+++ hypr/.config/hypr/monitors.lua
@@ -6,7 +6,7 @@ hl.monitor({
 	output = "eDP-1",
 	mode = "2880x1800@120",
 	position = "auto",
-	scale = 1.333333,
+	scale = 1.5, --1.333333,
 })
 
 -- Fallback for external monitors / undocked states
