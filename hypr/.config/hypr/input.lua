e6d95ac0ad443bb5e49a22799ebbe6a5c87d13ea
feokuma <feokuma@gmail.com>
2026-09-18 18:28:11 -0300

    🔧 hypr: lower touchpad scroll factor
    Also restates the input block with tabs for consistency with the other Lua modules.

--- hypr/.config/hypr/input.lua
+++ hypr/.config/hypr/input.lua
@@ -2,21 +2,22 @@
 -- See https://wiki.hypr.land/Configuring/Core/Devices/
 
 hl.config({
-    input = {
-        kb_layout  = "us",
-        kb_variant = "intl",
+	input = {
+		kb_layout = "us",
+		kb_variant = "intl",
 
-        follow_mouse = 1,
+		follow_mouse = 1,
 
-        touchpad = {
-            natural_scroll = true,
-            tap_to_click   = false,
-        },
-    },
+		touchpad = {
+			natural_scroll = true,
+			tap_to_click = false,
+			scroll_factor = 0.1,
+		},
+	},
 })
 
 hl.gesture({
-    fingers   = 3,
-    direction = "horizontal",
-    action    = "workspace",
+	fingers = 3,
+	direction = "horizontal",
+	action = "workspace",
 })
